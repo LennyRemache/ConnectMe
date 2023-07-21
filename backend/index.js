95d878396f0c02101e7560ec93e87a67deb717b9
LennyRemache <lenny.j.remache@gmail.com>
2023-07-21 19:09:01 -0400

    Applied proper middleware to allow server to run without error

--- backend/index.js
+++ backend/index.js
@@ -4,4 +4,12 @@ const cors = require("cors");
 const app = express(); // initializing express server side application
 
 // connecting server to the database
-require("./db/mongoose"); // needed when recieving data from front-end into the back-end that will be pushed into the db vice versa
+//require("./db/mongoose"); // needed when recieving data from front-end into the back-end that will be pushed into the db vice versa
+
+// needed to fix error where post request body is undefined and causes validationError
+app.use(express.json());
+app.use(cors()); // makes it possible to make api calls from the client side
+
+app.listen(3001, (req, res) => {
+  console.log("Server Running at Port 3001");
+});
