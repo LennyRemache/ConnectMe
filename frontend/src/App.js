6a8b4a3a6b93c1bf4fa70b7fce1091426bb1765c
LennyRemache <lenny.j.remache@gmail.com>
2023-08-10 16:43:07 -0400

    Removed react bootstrap and made own navbar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,16 +1,12 @@
 import { Routes, Route, NavLink } from "react-router-dom";
 import "./App.css";
 //import { motion } from "framer-motion";
-import Form from "./components/AccountForms/Form";
 import NavBar from "./components/NavBar";
 
 function App() {
   return (
     <div className="App">
       <NavBar />
-      <Routes>
-        <Route path="form/*" element={<Form />} />
-      </Routes>
     </div>
   );
 }
--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -1,41 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 
-import Container from "react-bootstrap/Container";
-import Nav from "react-bootstrap/Nav";
-import Navbar from "react-bootstrap/Navbar";
-import NavDropdown from "react-bootstrap/NavDropdown";
+import { NavLink, Routes, Route, useNavigate } from "react-router-dom";
+import "../styles/NavBar.css";
+import Form from "../components/AccountForms/Form.jsx";
 
 function NavBar() {
   return (
-    <Navbar collapseOnSelect expand="lg" className="bg-body-tertiary">
-      <Container>
-        <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
-        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-        <Navbar.Collapse id="responsive-navbar-nav">
-          <Nav className="me-auto">
-            <Nav.Link href="#features">Features</Nav.Link>
-            <Nav.Link href="#pricing">Pricing</Nav.Link>
-            <NavDropdown title="Dropdown" id="collasible-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">
-                Another action
-              </NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.4">
-                Separated link
-              </NavDropdown.Item>
-            </NavDropdown>
-          </Nav>
-          <Nav>
-            <Nav.Link href="#deets">More deets</Nav.Link>
-            <Nav.Link eventKey={2} href="#memes">
-              Dank memes
-            </Nav.Link>
-          </Nav>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
+    <>
+      <nav className="mainNav">
+        <div className="leftNav">
+          <NavLink>ConnectMe</NavLink>
+          <NavLink>Templates</NavLink>
+          <NavLink>Pricing</NavLink>
+        </div>
+        <div className="rightNav">
+          <NavLink to="form">Sign In</NavLink>
+          <NavLink>More</NavLink>
+        </div>
+      </nav>
+      <Routes>
+        <Route path="form/*" element={<Form />} />
+      </Routes>
+    </>
   );
 }
 
