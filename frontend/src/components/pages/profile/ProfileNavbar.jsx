985fd592d87830c6bbbb7791d78cc1af420dc124
LennyRemache <lenny.j.remache@gmail.com>
2023-08-17 22:24:34 -0400

    Fixed build errors

--- frontend/src/components/pages/profile/ProfileNavbar.jsx
+++ frontend/src/components/pages/profile/ProfileNavbar.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import { NavLink } from "react-router-dom";
 import { profileItems } from "./ProfileItems";
 import "../../../styles/pages/profile/ProfileNavbar.css";
