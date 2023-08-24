770dc19986f6cdc5be1a859dcc8e2b51b085e5c9
LennyRemache <lenny.j.remache@gmail.com>
2023-08-23 22:10:22 -0400

    Styled appearance profile section

--- frontend/src/components/pages/profile/Appearance/Profile.jsx
+++ frontend/src/components/pages/profile/Appearance/Profile.jsx
@@ -1,23 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
 import "../../../../styles/pages/profile/Appearance/Profile.css";
 
 function Profile() {
+  const [bio, setBio] = useState("");
   return (
     <>
       <div className="appearance-profile-container">
         <h1>Profile</h1>
         <div className="appearance-profile">
           <div className="appearance-pic">
-            <div className="appearance-pic-img"></div>
+            <div>
+              <div className="appearance-pic-img"></div>
+            </div>
             <div className="appearance-pic-btns">
               <button className="appearance-pic-btn pick">Pick an image</button>
               <button className="appearance-pic-btn remove">Remove</button>
             </div>
           </div>
+          <div className="appearance-title">
+            <input type="text" name="" id="" />
+            <label>Profile Title</label>
+          </div>
+          <div className="appearance-bio">
+            <textarea
+              name=""
+              id=""
+              cols="30"
+              rows="4"
+              value={bio}
+              onChange={(e) => setBio(e.target.value)}
+            ></textarea>
+            <label className="appearance-bio-label">Bio</label>
+            <label className="appearance-bio-count">{bio.length} / 80</label>
+          </div>
         </div>
-      </div>
-      <div className="appearance-buttons">
-        <div className="appearance-buttons-content"></div>
       </div>
     </>
   );
