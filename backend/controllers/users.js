10145996b60cff2094c709a80dd1cd1137f660e5
LennyRemache <lenny.j.remache@gmail.com>
2023-08-29 19:39:27 -0400

    Allowing user selection based on id on users route

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -3,6 +3,7 @@ import { User } from "../models/user.js";
 export const getUser = async (req, res) => {
   try {
     const user = await User.findById(req.params.id);
+    console.log(user);
     res.status(201).json({ user });
   } catch (err) {
     res.status(400).json({ err: err.message });
