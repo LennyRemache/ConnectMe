649a03ae5dd97ee140f1b6aa2b5aa78c9df3d81f
LennyRemache <lenny.j.remache@gmail.com>
2023-08-31 19:38:57 -0400

    Updated routes to use username instead of user id to retrieve db data

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -4,7 +4,7 @@ import { getUser, updateUser } from "../controllers/users.js";
 const router = express.Router();
 
 // User based on ID
-router.get("/:id", getUser);
-router.put("/:id", updateUser);
+router.get("/:userName", getUser);
+router.put("/:userName", updateUser);
 
 export default router;
