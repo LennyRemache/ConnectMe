55240de5e02b4106939ae67d320d2bc735afc463
LennyRemache <lenny.j.remache@gmail.com>
2023-09-04 18:59:49 -0400

    Updated route paths to identify profile title and bio updates

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -9,11 +9,23 @@ export const getUser = async (req, res) => {
   }
 };
 
-export const updateUser = async (req, res) => {
+export const updateTitle = async (req, res) => {
   try {
     const { title } = req.body;
     const user = await User.findOne({ userName: req.params.userName });
     user.profile.appearance.title = title;
+    await user.save();
+    res.status(201).json({ user });
+  } catch (err) {
+    res.status(400).json({ err: err.message });
+  }
+};
+
+export const updateBio = async (req, res) => {
+  try {
+    const { bio } = req.body;
+    const user = await User.findOne({ userName: req.params.userName });
+    user.profile.appearance.bio = bio;
     await user.save();
     res.status(201).json({ user });
   } catch (err) {
--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,10 +1,11 @@
 import express from "express";
-import { getUser, updateUser } from "../controllers/users.js";
+import { getUser, updateTitle, updateBio } from "../controllers/users.js";
 
 const router = express.Router();
 
 // User based on ID
 router.get("/:userName", getUser);
-router.put("/:userName", updateUser);
+router.put("/:userName/title", updateTitle);
+router.put("/:userName/bio", updateBio);
 
 export default router;
