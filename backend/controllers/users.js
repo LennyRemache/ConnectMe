d0a3ddd128c1ab396919502fa760c98b10ddb7f3
LennyRemache <lenny.j.remache@gmail.com>
2023-09-05 21:31:46 -0400

    Checking what the request file object contains

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -37,7 +37,7 @@ export const updatePicture = async (req, res) => {
   try {
     const user = await User.findOne({ userName: req.params.userName });
     const imagePath = req.file;
-    user.profile.appearance.picturePath = imagePath;
+    user.profile.appearance.picturePath = `${imagePath}`;
     await user.save();
     res.status(201).json({ user });
   } catch (err) {
