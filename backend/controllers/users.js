9d162700b03f3cd5f53e4edfdf71f793b77a2541
LennyRemache <lenny.j.remache@gmail.com>
2023-09-05 23:14:32 -0400

    Attempting to fix cloudinary image upload

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -39,7 +39,7 @@ export const updatePicture = async (req, res) => {
     const imagePath = req.file;
     user.profile.appearance.picturePath = `${imagePath}`;
     await user.save();
-    res.status(201).json({ user });
+    res.status(201).json(req.body);
   } catch (err) {
     res.status(400).json({ err: err.message });
   }
