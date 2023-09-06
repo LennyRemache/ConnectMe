3b9925d75c2dbcaccab20dde66251f6c2e40c5ac
LennyRemache <lenny.j.remache@gmail.com>
2023-09-05 23:09:43 -0400

    Attempting to fix cloudinary image upload

--- backend/cloudinary/multer.js
+++ backend/cloudinary/multer.js
@@ -1,19 +1,21 @@
 import multer from "multer";
 import cloudinary from "cloudinary";
-import cloudinaryStorage from "multer-storage-cloudinary";
+import { CloudinaryStorage } from "multer-storage-cloudinary";
 
 // a multer storage engine for Cloudinary
-const storage = cloudinaryStorage({
-  folder: "profile_pics",
-  allowedFormats: ["jpg", "png"],
-  transformation: [
-    {
-      width: 500,
-      height: 500,
-      crop: "limit",
-    },
-  ],
+const storage = new CloudinaryStorage({
   cloudinary: cloudinary, // A Cloudinary API object configured in config file
+  params: {
+    folder: "profile_pics",
+    allowedFormats: ["jpg", "png"],
+    transformation: [
+      {
+        width: 500,
+        height: 500,
+        crop: "limit",
+      },
+    ],
+  },
 });
 
 export const upload = multer({ storage: storage }); // middleware for uploading files using multer
